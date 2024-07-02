b09e7f46df6cd7695f023193782307a07bf08ee0
min697 <minsang3497@naver.com>
2024-07-02 17:25:14 +0900

    Update 퀴즈2.py

--- 퀴즈2.py
+++ 퀴즈2.py
@@ -1,4 +1,4 @@
 exchage = {'달러':1320, '엔화':950, '위안':182}
 chul = [13,200,13]
-
-print(exchage['달러'] * chul[0] + exchage['엔화'] * chul[1] + exchage['위안'] * chul[2])
+total = (exchage['달러'] * chul[0] + exchage['엔화'] * chul[1] + exchage['위안'] * chul[2])
+print('가지고 계신 돈은', total, '원 입니다.')

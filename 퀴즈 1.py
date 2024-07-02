a9318462391ac94c15c675422a4a92b1f12f79b7
min697 <minsang3497@naver.com>
2024-07-02 17:21:52 +0900

    퀴즈1

---
+++ 퀴즈 1.py
@@ -0,0 +1,18 @@
+#사람 1
+name1 = input("이름을 입력하세요.")
+age1 = input("나이를 입력하세요.")
+mail1 = input("이메일 주소를 입력하세요.")
+phone1 = input("연락처를 입력하세요")
+
+#사람 2
+name2 = input("이름을 입력하세요.")
+age2 = input("나이를 입력하세요.")
+mail2 = input("이메일 주소를 입력하세요.")
+phone2 = input("연락처를 입력하세요")
+
+dic_1 = {name1:['나이1':age1,'메일1':mail1,'연락처1':phone1]}
+dic_2 = {name2:{'나이2':age2,'메일1':mail2,'연락처1':phone2}}
+print(dic_1)
+print(dic_2)
+
+
---
+++ 퀴즈2.py
@@ -0,0 +1,4 @@
+exchage = {'달러':1320, '엔화':950, '위안':182}
+chul = [13,200,13]
+
+print(exchage['달러'] * chul[0] + exchage['엔화'] * chul[1] + exchage['위안'] * chul[2])
